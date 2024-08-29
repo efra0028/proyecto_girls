d4c19581691f642e121afc55dcadca245cb5d88c
efra0028 <efrach28.com>
2024-08-28 22:02:24 -0400

    tablas

---
+++ database/migrations/2024_08_28_175020_create_receipts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('record_id')->nullable(); 
+            $table->date('fecha_emision'); 
+            $table->decimal('monto', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('receipts');
+    }
+};
